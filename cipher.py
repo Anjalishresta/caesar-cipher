47766088430f2fc67fd869f1f095c9b75bfdfa42
Anjali Shrestha <avipsashrestha1993@gmail.com>
2024-11-04 21:39:36 -0500

    Ceaser Cipher code

--- cipher.py
+++ cipher.py
@@ -1,1 +1,17 @@
 # add your code here
+def caesar_cipher(text, shift=5):
+    encrypted_text = 
+
+    for char in text:
+        if char.isalpha():
+            ascii_offset = 65 if char.isupper() else 97
+            new_char = chr(((ord(char) - ascii_offset + shift) % 26) + ascii_offset)
+            encrypted_text += new_char
+        else:
+            encrypted_text += char
+
+    return encrypted_text
+
+text_input = input(Please enter a sentence: )
+encrypted_output = caesar_cipher(text_input)
+print(The encrypted sentence is:, encrypted_output)
